b18e939eda94957a0ad875fce340196b38f42dd7
tmoSolutions <94002135+tmoSolutions@users.noreply.github.com>
2021-11-25 12:30:01 +0100

    rename main.qml

--- apps/NewsFeedReader/main.cpp
+++ apps/NewsFeedReader/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
     QScopedPointer<QQmlApplicationEngine> engine(new QQmlApplicationEngine);
     engine->addImportPath(":/");
 
-    engine->load(QUrl("qrc:/Mainpage.qml"));
+    engine->load(QUrl("qrc:/main.qml"));
 
     return app.exec();
 }
