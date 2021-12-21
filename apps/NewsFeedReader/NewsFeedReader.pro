724e6d43b2920753b08ae93cbd206d6edbd2e1bf
femto-code <59444046+femto-code@users.noreply.github.com>
2021-12-21 12:10:44 +0100

    Initialized FeedList module in QT Creator

--- apps/NewsFeedReader/NewsFeedReader.pro
+++ apps/NewsFeedReader/NewsFeedReader.pro
@@ -4,6 +4,7 @@ QT += network
 
 #Include modules here
 include($$modules_dir/Feed/Feed.pri)
+include($$modules_dir/FeedList/FeedList.pri)
 include($$modules_dir/NetworkMgr/NetworkMgr.pri)
 
 #Include project files
---
+++ modules/FeedList/FeedList.h
@@ -0,0 +1,19 @@
+#ifndef FEEDLIST_H
+#define FEEDLIST_H
+
+#include <QObject>
+#include <QGuiApplication>
+#include <QtQuick>
+
+using namespace std;
+
+class FeedList : public QObject {
+
+    Q_OBJECT
+
+
+public:
+ 
+};
+
+#endif
