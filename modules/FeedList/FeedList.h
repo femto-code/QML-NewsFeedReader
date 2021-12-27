bc2d439fd5218c8ea2c0080130d1aa5174f54343
femto-code <59444046+femto-code@users.noreply.github.com>
2021-12-27 18:52:37 +0100

    Implemented FeedList class

--- modules/FeedList/FeedList.h
+++ modules/FeedList/FeedList.h
@@ -1,19 +1,29 @@
 #ifndef FEEDLIST_H
 #define FEEDLIST_H
 
+#include "Feed.h"
 #include <QObject>
-#include <QGuiApplication>
-#include <QtQuick>
+#include <QQmlListProperty>
+#include <Qlist>
 
-using namespace std;
+typedef QList<Feed*> FeedSources;
 
 class FeedList : public QObject {
 
     Q_OBJECT
+    Q_PROPERTY(QQmlListProperty<Feed> feedSources READ feedSources NOTIFY feedSourcesChanged)
 
+    FeedSources m_FeedSources;
 
 public:
- 
+    FeedList(QObject* parent = nullptr);
+    QQmlListProperty<Feed> feedSources();
+
+public slots:
+    void add(QString name);
+
+signals:
+    void feedSourcesChanged();
 };
 
 #endif
